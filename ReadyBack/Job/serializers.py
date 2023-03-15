174d3fc39024674a557b98316ac4319c60dd6aa9
Ubuntu <ubuntu@ip-172-31-42-31.eu-central-1.compute.internal>
2023-03-15 15:20:07 +0000

    add serializer for job attachemnt

--- ReadyBack/Job/serializers.py
+++ ReadyBack/Job/serializers.py
@@ -1,11 +1,11 @@
 from rest_framework import serializers
-from Job.models import Expected_Duration, Job, Other_Skills
+from Job.models import Expected_Duration, Job, Other_Skills, Job_Attachment
 
 
 class JobSerialize(serializers.ModelSerializer):
     class Meta:
         model = Job
-        fields = ('id', 'description', 'payment_amount', 'ExpectedDurationId', 'ClientId', 'MainSkillId', 'PaymentTypeId')
+        fields = ('id', 'title', 'description', 'is_contest', 'is_hourly', 'is_recruiter_project', 'currency_type', 'payment_amount', 'ExpectedDurationId', 'ClientId', 'MainSkillId', 'PaymentTypeId')
 
 class ExpectedDurationSerialize(serializers.ModelSerializer):
     class Meta:
@@ -16,3 +16,8 @@ class OtherSkillsSerialize(serializers.ModelSerializer):
     class Meta:
         model = Other_Skills
         fields = ('id', 'SkillId', 'JobId')
+
+class JobAttachmentSerialize(serializers.ModelSerializer):
+    class Meta:
+        model = Job_Attachment
+        fields = ('id', 'attachment_link', 'JobId')
