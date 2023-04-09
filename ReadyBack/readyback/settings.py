9aa29772488d9f3ddd7aaeb57b557505c087b088
Ubuntu <ubuntu@ip-172-31-42-31.eu-central-1.compute.internal>
2023-04-09 22:49:37 +0000

    fix

--- ReadyBack/readyback/settings.py
+++ ReadyBack/readyback/settings.py
@@ -231,13 +231,6 @@ TWILIO_ACCOUNT_SID = os.environ.get('TWILIO_ACCOUNT_SID')
 TWILIO_AUTH_TOKEN = os.environ.get('TWILIO_AUTH_TOKEN')
 TWILIO_PHONE_NUMBER = os.environ.get('TWILIO_PHONE_NUMBER')
 
-STRIPE_LIVE_SECRET_KEY = os.environ.get("STRIPE_LIVE_SECRET_KEY", "<your secret key>")
-STRIPE_TEST_SECRET_KEY = os.environ.get("STRIPE_TEST_SECRET_KEY", "sk_test_")
-STRIPE_LIVE_MODE = False  # Change to True in production
-DJSTRIPE_WEBHOOK_SECRET = "whsec_xxx"  # Get it from the section in the Stripe dashboard where you added the webhook endpoint
-DJSTRIPE_USE_NATIVE_JSONFIELD = True  # We recommend setting to True for new installations
-DJSTRIPE_FOREIGN_KEY_TO_FIELD = "id"
-
 ACCOUNT_ADAPTER = 'User.adapter.DefaultAccountAdapter'
 if DEBUG:
     URL_FRONT = 'https://www.readymademe.com:3000/'
