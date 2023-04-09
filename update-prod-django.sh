0b1fea04994de5ee3019ec43506dc90e822cf23f
Ubuntu <ubuntu@ip-172-31-42-31.eu-central-1.compute.internal>
2023-04-09 04:39:09 +0000

    fix

--- update-prod-django.sh
+++ update-prod-django.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 echo "[START]"
-echo "---stopping django---"
-docker compose -f docker-compose.yml -f docker-compose.prod.yml stop django
+echo "---stopping app---"
+docker compose -f docker-compose.yml -f docker-compose.prod.yml stop app
 echo "---removing containers---"
 docker compose rm -f
 echo "---removing stale volumes---"
 docker volume prune -f
-echo "---rebuilding django---"
-docker compose build django
-echo "---restarting django---"
-docker compose -f docker-compose.yml -f docker-compose.prod.yml up -d --no-deps django
+echo "---rebuilding app---"
+docker compose build app
+echo "---restarting app---"
+docker compose -f docker-compose.yml -f docker-compose.prod.yml up -d --no-deps app
 echo "[END]"
