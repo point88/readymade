ecd30fed9db7ad70e22e3a3012098220330763e9
Ubuntu <ubuntu@ip-172-31-42-31.eu-central-1.compute.internal>
2023-04-10 12:47:12 +0000

    fix

--- ReadyBack/readyback/settings.py
+++ ReadyBack/readyback/settings.py
@@ -28,13 +28,7 @@ DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 CORS_ORIGIN_ALLOW_ALL = True
-CORS_ALLOW_ALL_ORIGINS = True
-CORS_ALLOW_CREDENTIALS = False
 
-CORS_ALLOWED_ORIGINS = [
-    'http://localhost:3000',
-    'https://localhost:3000',
-]
 
 # Application definition
 
